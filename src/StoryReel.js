0994114080edae402048fc23311fe40fe16273fd
dusenaberawilliam <dusenaberawilliam07@gmail.com>
2021-11-14 17:52:53 +0200

    sidebar and feed

---
+++ src/StoryReel.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Story from './Story'
+import './StoryReel.css'
+import kglOne from './assets/kglOne.jpeg'
+import kglTwo from './assets/kglTwo.jpg'
+
+const StoryReel = () => {
+    return (
+        <div className="storyReel">
+            <Story image={kglOne} profileSrc={kglTwo} title="William" />
+            <Story image={kglOne} profileSrc={kglTwo} title="William" />
+            <Story image={kglOne} profileSrc={kglTwo} title="William" />
+            <Story image={kglOne} profileSrc={kglTwo} title="William" />
+            <Story image={kglOne} profileSrc={kglTwo} title="William" />
+            <Story image={kglOne} profileSrc={kglTwo} title="William" />
+        </div>
+    )
+}
+
+export default StoryReel
