e3e7f3a9c76971767ee10a81809572048f1dfcc0
dusenaberawilliam <dusenaberawilliam07@gmail.com>
2021-11-14 02:52:09 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDwMgNV6f_1BCJjeDsf4iYMMG2uV3U4gY",
+    authDomain: "fb-clone-3f0a2.firebaseapp.com",
+    projectId: "fb-clone-3f0a2",
+    storageBucket: "fb-clone-3f0a2.appspot.com",
+    messagingSenderId: "873511789345",
+    appId: "1:873511789345:web:636dea4bc3709215219460"
+};
